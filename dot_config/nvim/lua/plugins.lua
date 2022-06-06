dd4ea700612a0a69b6f9d7ab9e1cd29f0fe5f937
Ivo Murrell <ivo.murrell@ft.com>
2022-06-06 23:29:32 +0100

    Add vim plugins from work laptop

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -2,4 +2,7 @@ require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
   use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
   use 'tpope/vim-commentary' -- Comment out lines
+  use 'machakann/vim-highlightedyank' -- Highlight line when yanking
+  use 'machakann/vim-sandwich' -- Add surroundings to text objects
+  use 'justinmk/vim-sneak' -- Jump to location with two characters
 end)
