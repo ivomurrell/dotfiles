f84ea80c2641a52d1d22c456b431dac705ef491c
Ivo Murrell <ivo.murrell@ft.com>
2023-09-17 14:10:16 +0100

    Declare module keymappings in lazy so they can be lazy-loaded

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -43,38 +43,3 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
-
-vim.keymap.set('n', '<space><space>',
-  function()
-    require('telescope.builtin').find_files({
-      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" } })
-  end
-)
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
-vim.keymap.set('n', '<leader>*', require('telescope.builtin').grep_string)
-vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers)
-vim.keymap.set('n', '<leader>ft', require('telescope.builtin').lsp_dynamic_workspace_symbols)
-vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags)
-
-vim.keymap.set('n', 'ss', '<Plug>Sneak_s')
-vim.keymap.set('n', 'SS', '<Plug>Sneak_S')
-vim.keymap.set({ 'o', 'x' }, 'z', '<Plug>Sneak_s')
-vim.keymap.set({ 'o', 'x' }, 'Z', '<Plug>Sneak_S')
-vim.keymap.set('n', 'f', '<Plug>Sneak_f')
-vim.keymap.set('n', 'F', '<Plug>Sneak_F')
-vim.keymap.set('n', 't', '<Plug>Sneak_t')
-vim.keymap.set('n', 'T', '<Plug>Sneak_T')
-
-vim.keymap.set('n', '<C-n>', '<cmd>NvimTreeToggle<cr>')
-vim.keymap.set('n', '<leader>r', '<cmd>NvimTreeRefresh<cr>')
-vim.keymap.set('n', '<leader>n', '<cmd>NvimTreeFindFile<cr>')
-
-vim.keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>')
-vim.keymap.set('n', '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>')
-vim.keymap.set('n', '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>')
-
-vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
-vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
-vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
-vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,195 +1,237 @@
 return {
-  'folke/lazy.nvim',                               -- Package manager
-  { 'lewis6991/impatient.nvim', enabled = false }, -- Implements caching to speed up startup
-  {
-    "dstein64/vim-startuptime",
-    cmd = "StartupTime",
-    init = function()
-      vim.g.startuptime_tries = 10
-    end,
-  },
-  {
-    'jose-elias-alvarez/null-ls.nvim',
-    enabled = false,
-    dependencies = { 'nvim-lua/plenary.nvim' },
-  }, -- add LSP support for non-LSP tools
-  {
-    'j-hui/fidget.nvim',
-    tag = 'legacy',
-    config = true,
-  }, -- show LSP loading progress
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = { "typescript", "javascript", "lua", "rust" },
-        highlight = {
-          enable = true
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["as"] = "@statement.outer",
-              ["a?"] = "@conditional.outer",
-              ["i?"] = "@conditional.inner",
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-              ["ia"] = "@parameter.inner",
-              ["aa"] = "@parameter.outer",
-              ["i/"] = "@regex.inner",
-              ["a/"] = "@regex.outer",
-            },
-          }
-        },
-        matchup = {
-          enable = true
-        }
-      }
-    end
-  },                                                 -- Better/faster syntax highlighting with treesitter
-  'nvim-treesitter/nvim-treesitter-textobjects',     -- Add treesitter groups as textobjects
-  'andymass/vim-matchup',                            -- Improve %-jumping with treesitter integration
-  'folke/tokyonight.nvim',                           -- Colour scheme that supports other plugins
-  'tpope/vim-commentary',                            -- Comment out lines
-  'tpope/vim-sleuth',                                -- Detect indentation
-  'tpope/vim-unimpaired',                            -- [ and ] shortcuts
-  'tpope/vim-fugitive',                              -- Git plugin
-  { 'tpope/vim-rhubarb',        enabled = false },   -- GitHub support for fugitive.vim
-  'machakann/vim-highlightedyank',                   -- Highlight line when yanking
-  'machakann/vim-sandwich',                          -- Add surroundings to text objects
-  'justinmk/vim-sneak',                              -- Jump to location with two characters
-  'chrisgrieser/nvim-spider',                        -- Respect CamelCase with word motions
-  'tommcdo/vim-lion',                                -- Aligning text
-  'romainl/vim-cool',                                -- Disables search highlighting once I'm done
-  { 'windwp/nvim-autopairs',        config = true }, -- Insert closing characters for pairs
-  { 'kyazdani42/nvim-web-devicons', lazy = true,  config = true },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'kyazdani42/nvim-web-devicons' },
-    config = function()
-      local function indentation()
-        local indent_type = vim.opt.expandtab:get() and 'spaces' or 'tabs'
-        return indent_type .. ': ' .. vim.opt.shiftwidth:get()
-      end
+	'folke/lazy.nvim',                              -- Package manager
+	{ 'lewis6991/impatient.nvim', enabled = false }, -- Implements caching to speed up startup
+	{
+		"dstein64/vim-startuptime",
+		cmd = "StartupTime",
+		init = function()
+			vim.g.startuptime_tries = 10
+		end,
+	},
+	{
+		'jose-elias-alvarez/null-ls.nvim',
+		enabled = false,
+		dependencies = { 'nvim-lua/plenary.nvim' },
+	}, -- add LSP support for non-LSP tools
+	{
+		'j-hui/fidget.nvim',
+		tag = 'legacy',
+		config = true,
+	}, -- show LSP loading progress
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+		config = function()
+			require('nvim-treesitter.configs').setup {
+				ensure_installed = { "typescript", "javascript", "lua", "rust" },
+				highlight = {
+					enable = true
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						-- Automatically jump forward to textobj, similar to targets.vim
+						lookahead = true,
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["as"] = "@statement.outer",
+							["a?"] = "@conditional.outer",
+							["i?"] = "@conditional.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["ia"] = "@parameter.inner",
+							["aa"] = "@parameter.outer",
+							["i/"] = "@regex.inner",
+							["a/"] = "@regex.outer",
+						},
+					}
+				},
+				matchup = {
+					enable = true
+				}
+			}
+		end
+	},                                              -- Better/faster syntax highlighting with treesitter
+	'nvim-treesitter/nvim-treesitter-textobjects',  -- Add treesitter groups as textobjects
+	'andymass/vim-matchup',                         -- Improve %-jumping with treesitter integration
+	'folke/tokyonight.nvim',                        -- Colour scheme that supports other plugins
+	'tpope/vim-commentary',                         -- Comment out lines
+	'tpope/vim-sleuth',                             -- Detect indentation
+	'tpope/vim-unimpaired',                         -- [ and ] shortcuts
+	'tpope/vim-fugitive',                           -- Git plugin
+	{ 'tpope/vim-rhubarb',        enabled = false }, -- GitHub support for fugitive.vim
+	'machakann/vim-highlightedyank',                -- Highlight line when yanking
+	'machakann/vim-sandwich',                       -- Add surroundings to text objects
+	{
+		'justinmk/vim-sneak',
+		keys = {
+			{ 'ss', '<Plug>Sneak_s' },
+			{ 'SS', '<Plug>Sneak_S' },
+			{ 'z',  '<Plug>Sneak_s', mode = { 'o', 'x' } },
+			{ 'Z',  '<Plug>Sneak_S', mode = { 'o', 'x' } },
+			{ 'f',  '<Plug>Sneak_f' },
+			{ 'F',  '<Plug>Sneak_F' },
+			{ 't',  '<Plug>Sneak_t' },
+			{ 'T',  '<Plug>Sneak_T' },
+		}
+	}, -- Jump to location with two characters
+	{
+		'chrisgrieser/nvim-spider',
+		keys = {
+			{ "w",  "<cmd>lua require('spider').motion('w')<CR>",  mode = { "n", "o", "x" }, { desc = "Spider-w" } },
+			{ "e",  "<cmd>lua require('spider').motion('e')<CR>",  mode = { "n", "o", "x" }, { desc = "Spider-e" } },
+			{ "b",  "<cmd>lua require('spider').motion('b')<CR>",  mode = { "n", "o", "x" }, { desc = "Spider-b" } },
+			{ "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" }, { desc = "Spider-ge" } },
+		},
+	},                                                -- Respect CamelCase with word motions
+	'tommcdo/vim-lion',                               -- Aligning text
+	'romainl/vim-cool',                               -- Disables search highlighting once I'm done
+	{ 'windwp/nvim-autopairs',        config = true }, -- Insert closing characters for pairs
+	{ 'kyazdani42/nvim-web-devicons', lazy = true,  config = true },
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'kyazdani42/nvim-web-devicons' },
+		config = function()
+			local function indentation()
+				local indent_type = vim.opt.expandtab:get() and 'spaces' or 'tabs'
+				return indent_type .. ': ' .. vim.opt.shiftwidth:get()
+			end
 
-      require('lualine').setup {
-        sections = {
-          lualine_c = { { 'filename', path = 1 } },
-          lualine_x = { indentation, 'filetype' },
-        },
-        extensions = { 'fugitive', 'nvim-tree' }
-      }
-    end
-  }, -- Customisable status line
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      current_line_blame = false,
-      current_line_blame_opts = {
-        virt_text_pos = 'right_align',
-        delay = 400
-      },
-      on_attach = function(bufnr)
-        vim.keymap.set('n', ']c', function()
-          if vim.wo.diff then return ']c' end
-          vim.schedule(function() require('gitsigns').next_hunk() end)
-          return '<Ignore>'
-        end, { buffer = bufnr, expr = true })
-        vim.keymap.set('n', '[c', function()
-          if vim.wo.diff then return '[c' end
-          vim.schedule(function() require('gitsigns').prev_hunk() end)
-          return '<Ignore>'
-        end, { buffer = bufnr, expr = true })
+			require('lualine').setup {
+				sections = {
+					lualine_c = { { 'filename', path = 1 } },
+					lualine_x = { indentation, 'filetype' },
+				},
+				extensions = { 'fugitive', 'nvim-tree' }
+			}
+		end
+	}, -- Customisable status line
+	{
+		'lewis6991/gitsigns.nvim',
+		opts = {
+			current_line_blame = false,
+			current_line_blame_opts = {
+				virt_text_pos = 'right_align',
+				delay = 400
+			},
+			on_attach = function(bufnr)
+				vim.keymap.set('n', ']c', function()
+					if vim.wo.diff then return ']c' end
+					vim.schedule(function() require('gitsigns').next_hunk() end)
+					return '<Ignore>'
+				end, { buffer = bufnr, expr = true })
+				vim.keymap.set('n', '[c', function()
+					if vim.wo.diff then return '[c' end
+					vim.schedule(function() require('gitsigns').prev_hunk() end)
+					return '<Ignore>'
+				end, { buffer = bufnr, expr = true })
 
-        vim.keymap.set({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-        vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk)
-        vim.keymap.set('n', '<leader>tb', require('gitsigns').toggle_current_line_blame)
+				vim.keymap.set({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+				vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk)
+				vim.keymap.set('n', '<leader>tb', require('gitsigns').toggle_current_line_blame)
 
-        vim.keymap.set({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-      end
-    }
-  }, -- Pretty git gutter and in-line blame
-  {
-    'kyazdani42/nvim-tree.lua',
-    dependencies = 'kyazdani42/nvim-web-devicons',
-    cmd = { 'NvimTreeToggle', 'NvimTreeFindFile' },
-    opts = { actions = { open_file = { quit_on_open = true } } }
-  }, -- Graphical file explorer
-  {
-    'akinsho/bufferline.nvim',
-    version = "*",
-    dependencies = { 'kyazdani42/nvim-web-devicons' },
-    config = true
-  },                          -- Tabs in a buffer line
-  { 'ojroques/nvim-bufdel' }, -- Don't close window when deleting buffers
-  {
-    'folke/trouble.nvim',
-    dependencies = { 'kyazdani42/nvim-web-devicons' },
-    config = true
-  },                                                                                        -- List diagnositic errors
-  { 'mfussenegger/nvim-dap', enabled = false },                                             -- Debugging tooling
-  { 'rcarriga/nvim-dap-ui',  enabled = false, dependencies = { 'mfussenegger/nvim-dap' } }, -- UI for debugging
-  {
-    'nvim-telescope/telescope.nvim',
-    version = '^0.1.0',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'kyazdani42/nvim-web-devicons',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make'
-      }, -- Use fzf for searching with telescope
-    },
-    config = function()
-      local telescope = require('telescope')
-      telescope.load_extension('fzf')
-      telescope.setup({
-        defaults = {
-          mappings = {
-            i = {
-              ["<Down>"] = require('telescope.actions').cycle_history_next,
-              ["<Up>"] = require('telescope.actions').cycle_history_prev,
-            }
-          }
-        }
-      })
-    end
-  }, -- Fuzzy search for various lists such as project files
-  {
-    'TimUntersberger/neogit',
-    enabled = false,
-    dependencies = 'nvim-lua/plenary.nvim',
-    config = true
-  }, -- Magit clone for Neovim
-  {
-    'mhartington/formatter.nvim',
-    enabled = false,
-    ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'rust' },
-    config = function()
-      local prettier_files = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
-      local prettier_config = {}
-      for _, ft in pairs(prettier_files) do
-        prettier_config[ft] = { require('formatter.filetypes.' .. ft).prettier }
-      end
-      local rust_fmt = require('formatter.filetypes.rust').rustfmt()
-      rust_fmt.args = { ",--edition=2021" }
-      prettier_config['rust'] = { function()
-        return rust_fmt
-      end }
-      require('formatter').setup {
-        filetype = prettier_config
-      }
-    end
-  },                         -- Format source files
-  'stevearc/dressing.nvim',  -- Make input windows nicer
-  'smerrill/vcl-vim-plugin', -- VCL syntax support
-  'imsnif/kdl.vim',          -- KDL syntax support
-  'hjson/vim-hjson',         -- HJSON syntax support
+				vim.keymap.set({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+			end
+		}
+	}, -- Pretty git gutter and in-line blame
+	{
+		'kyazdani42/nvim-tree.lua',
+		dependencies = 'kyazdani42/nvim-web-devicons',
+		keys = {
+			{ '<C-n>',     '<cmd>NvimTreeToggle<cr>' },
+			{ '<leader>r', '<cmd>NvimTreeRefresh<cr>' },
+			{ '<leader>n', '<cmd>NvimTreeFindFile<cr>' }
+		},
+		opts = { actions = { open_file = { quit_on_open = true } } }
+	}, -- Graphical file explorer
+	{
+		'akinsho/bufferline.nvim',
+		version = "*",
+		dependencies = { 'kyazdani42/nvim-web-devicons' },
+		config = true
+	},                         -- Tabs in a buffer line
+	{ 'ojroques/nvim-bufdel' }, -- Don't close window when deleting buffers
+	{
+		'folke/trouble.nvim',
+		dependencies = { 'kyazdani42/nvim-web-devicons' },
+		keys = {
+			{ '<leader>xx', '<cmd>TroubleToggle<cr>' },
+			{ '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>' },
+			{ '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>' },
+		},
+		config = true
+	},                                                                                       -- List diagnositic errors
+	{ 'mfussenegger/nvim-dap', enabled = false },                                            -- Debugging tooling
+	{ 'rcarriga/nvim-dap-ui',  enabled = false, dependencies = { 'mfussenegger/nvim-dap' } }, -- UI for debugging
+	{
+		'nvim-telescope/telescope.nvim',
+		version = '^0.1.0',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'kyazdani42/nvim-web-devicons',
+			{
+				'nvim-telescope/telescope-fzf-native.nvim',
+				build = 'make'
+			}, -- Use fzf for searching with telescope
+		},
+		keys = {
+			{ '<space><space>',
+				function()
+					require('telescope.builtin').find_files({
+						find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" } })
+				end
+			},
+			{ '<leader>fg', '<cmd>Telescope live_grep<cr>' },
+			{ '<leader>*',  '<cmd>Telecope grep_string<cr>' },
+			{ '<leader>fb', '<cmd>Telecope buffers<cr>' },
+			{ '<leader>ft', '<cmd>Telecope lsp_dynamic_workspace_symbols<cr>' },
+			{ '<leader>fh', '<cmd>Telecope help_tags<cr>' },
+		},
+		config = function()
+			local telescope = require('telescope')
+			telescope.load_extension('fzf')
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<Down>"] = require('telescope.actions').cycle_history_next,
+							["<Up>"] = require('telescope.actions').cycle_history_prev,
+						}
+					}
+				}
+			})
+		end
+	}, -- Fuzzy search for various lists such as project files
+	{
+		'TimUntersberger/neogit',
+		enabled = false,
+		dependencies = 'nvim-lua/plenary.nvim',
+		config = true
+	}, -- Magit clone for Neovim
+	{
+		'mhartington/formatter.nvim',
+		enabled = false,
+		ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'rust' },
+		config = function()
+			local prettier_files = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
+			local prettier_config = {}
+			for _, ft in pairs(prettier_files) do
+				prettier_config[ft] = { require('formatter.filetypes.' .. ft).prettier }
+			end
+			local rust_fmt = require('formatter.filetypes.rust').rustfmt()
+			rust_fmt.args = { ",--edition=2021" }
+			prettier_config['rust'] = { function()
+				return rust_fmt
+			end }
+			require('formatter').setup {
+				filetype = prettier_config
+			}
+		end
+	},                        -- Format source files
+	'stevearc/dressing.nvim', -- Make input windows nicer
+	'smerrill/vcl-vim-plugin', -- VCL syntax support
+	'imsnif/kdl.vim',         -- KDL syntax support
+	'hjson/vim-hjson',        -- HJSON syntax support
 }
