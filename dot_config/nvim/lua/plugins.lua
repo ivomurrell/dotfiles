412e098292a6c7daacd3cd451c5200747e784f9c
Ivo Murrell <ivo.murrell@ft.com>
2024-07-09 15:43:39 +0100

    misc tidying up of nvim config

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,66 +1,6 @@
 return {
 	'folke/lazy.nvim', -- Package manager
 	{
-		'stevearc/conform.nvim',
-		event = 'BufWritePre',
-		cmd = { 'ConformInfo', 'FormatEnable', 'FormatDisable' },
-		opts = function()
-			local config = {
-				formatters_by_ft = {},
-				format_after_save = function(bufnr)
-					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-						return
-					end
-					return {
-						lsp_fallback = true,
-					}
-				end,
-				formatters = {
-					deno_fmt = {
-						cwd = require("conform.util").root_file({ "deno.json", "deno.jsonc" }),
-						require_cwd = true
-					}
-				}
-			}
-			local languages = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'css', 'scss', 'less',
-				'html', 'json', 'jsonc', 'yaml', 'markdown', 'markdown.mdx', 'graphql', 'handlebars' }
-			local deno_langs = { 'javascript', 'javascriptreact', 'json', 'jsonc', 'markdown', 'typescript', 'typescriptreact' }
-			local function supportedByDeno(ft)
-				for _, denolang in pairs(deno_langs) do
-					if denolang == ft then
-						return true
-					end
-				end
-			end
-			for _, lang in pairs(languages) do
-				config.formatters_by_ft[lang] = supportedByDeno(lang) and
-						{ { 'deno_fmt', 'prettierd', 'prettier' } } or
-						{ { 'prettierd', 'prettier' } }
-			end
-			return config
-		end,
-		config = function(_, opts)
-			require('conform').setup(opts)
-			vim.api.nvim_create_user_command("FormatDisable", function(args)
-				if args.bang then
-					-- FormatDisable! will disable formatting just for this buffer
-					vim.b.disable_autoformat = true
-				else
-					vim.g.disable_autoformat = true
-				end
-			end, {
-				desc = "Disable autoformat-on-save",
-				bang = true,
-			})
-			vim.api.nvim_create_user_command("FormatEnable", function()
-				vim.b.disable_autoformat = false
-				vim.g.disable_autoformat = false
-			end, {
-				desc = "Re-enable autoformat-on-save",
-			})
-		end
-	}, -- define non-LSP formatters
-	{
 		'j-hui/fidget.nvim',
 		tag = 'legacy',
 		event = 'VeryLazy',
@@ -153,16 +93,16 @@ return {
 		'windwp/nvim-autopairs',
 		event = 'InsertEnter',
 		config = true
-	}, -- Insert closing characters for pairs
-	{ 'kyazdani42/nvim-web-devicons', lazy = true, config = true },
+	},                                                             -- Insert closing characters for pairs
+	{ 'kyazdani42/nvim-web-devicons', lazy = true, config = true }, -- Colourful file icons using nerdfont
 	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'kyazdani42/nvim-web-devicons' },
 		event = "VeryLazy",
 		config = function()
 			local function indentation()
-				local indent_type = vim.opt.expandtab:get() and 'spaces' or 'tabs'
-				return indent_type .. ': ' .. vim.opt.shiftwidth:get()
+				local indent_type = vim.o.expandtab and 'spaces' or 'tabs'
+				return indent_type .. ': ' .. vim.o.shiftwidth
 			end
 
 			require('lualine').setup {
@@ -223,7 +163,7 @@ return {
 			{ '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>' },
 		},
 		config = true
-	}, -- List diagnositic errors
+	}, -- List diagnostic errors
 	{
 		'nvim-telescope/telescope.nvim',
 		version = '^0.1.0',
---
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,60 @@
+return {
+  'stevearc/conform.nvim',
+  event = 'BufWritePre',
+  cmd = { 'ConformInfo', 'FormatEnable', 'FormatDisable' },
+  opts = function()
+    local config = {
+      formatters_by_ft = {},
+      format_after_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return {
+          lsp_fallback = true,
+        }
+      end,
+      formatters = {
+        deno_fmt = {
+          cwd = require("conform.util").root_file({ "deno.json", "deno.jsonc" }),
+          require_cwd = true
+        }
+      }
+    }
+    local languages = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'css', 'scss', 'less',
+      'html', 'json', 'jsonc', 'yaml', 'markdown', 'markdown.mdx', 'graphql', 'handlebars' }
+    local deno_langs = { 'javascript', 'javascriptreact', 'json', 'jsonc', 'markdown', 'typescript', 'typescriptreact' }
+    local function supportedByDeno(ft)
+      for _, denolang in pairs(deno_langs) do
+        if denolang == ft then
+          return true
+        end
+      end
+    end
+    for _, lang in pairs(languages) do
+      config.formatters_by_ft[lang] = supportedByDeno(lang) and
+          { { 'deno_fmt', 'prettierd', 'prettier' } } or
+          { { 'prettierd', 'prettier' } }
+    end
+    return config
+  end,
+  config = function(_, opts)
+    require('conform').setup(opts)
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+  end
+} -- Define non-LSP formatters
